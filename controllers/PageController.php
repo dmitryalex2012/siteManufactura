1cbc9b9485bb008d53bdabd9d6f6908712d6bcdd
dmitryalex2012 <tpmfd27@gmail.com>
2019-08-05 23:54:27 +0300

    new FW

---
+++ controllers/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+/*
+ Controller for page site
+ */
+class PageController extends Controller
+{
+    /**
+     For products list page
+     */
+     public function actionListproducts()
+    {
+        return $this->render('listproducts');
+    }
+}
