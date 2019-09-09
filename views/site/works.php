d7bb60f67d0ba480ae15f4d0de800cfd2ce86bca
dmitryalex2012 <tpmfd27@gmail.com>
2019-09-09 17:55:27 +0300

    Insert Bootstrap "Carousel" for all "ourWorks".

--- views/site/works.php
+++ views/site/works.php
@@ -13,65 +13,115 @@
 ?>
 
 <div class="products">
+
+    <!--    <div class="row">-->
+    <!--        <div class="col-12 col-sm-5">-->
+    <!--            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">-->
+    <!--                <ol class="carousel-indicators">-->
+    <!--                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>-->
+    <!--                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>-->
+    <!--                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>-->
+    <!--                </ol>-->
+    <!--                <div class="carousel-inner">-->
+    <!--                    <div class="carousel-item active" data-interval="1000000">-->
+    <!--                        <img src="/foto/kitchen1.jpg" class="d-block w-100">-->
+    <!--                    </div>-->
+    <!--                    <div class="carousel-item" data-interval="1000000">-->
+    <!--                        <img src="/foto/kitchen2.jpg" class="d-block w-100">-->
+    <!--                    </div>-->
+    <!--                    <div class="carousel-item" data-interval="1000000">-->
+    <!--                        <img src="/foto/kitchen3.jpg" class="d-block w-100">-->
+    <!--                    </div>-->
+    <!--                </div>-->
+    <!--                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">-->
+    <!--                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+    <!--                    <span class="sr-only">Previous</span>-->
+    <!--                </a>-->
+    <!--                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">-->
+    <!--                    <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+    <!--                    <span class="sr-only">Next</span>-->
+    <!--                </a>-->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--    </div>-->
+    <!---->
+    <!--    <div class="row">-->
+    <!--        <div class="col-12 col-sm-5">-->
+    <!--            <div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel">-->
+    <!--                <ol class="carousel-indicators">-->
+    <!--                    <li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active"></li>-->
+    <!--                    <li data-target="#carouselExampleIndicators1" data-slide-to="1"></li>-->
+    <!--                    <li data-target="#carouselExampleIndicators1" data-slide-to="2"></li>-->
+    <!--                </ol>-->
+    <!--                <div class="carousel-inner">-->
+    <!--                    <div class="carousel-item active" data-interval="false">-->
+    <!--                        <img src="/foto/livingRoom1.jpg" class="d-block w-100">-->
+    <!--                    </div>-->
+    <!--                    <div class="carousel-item" data-interval="false">-->
+    <!--                        <img src="/foto/livingRoom2.jpg" class="d-block w-100">-->
+    <!--                    </div>-->
+    <!--                    <div class="carousel-item" data-interval="false">-->
+    <!--                        <img src="/foto/livingRoom3.jpg" class="d-block w-100">-->
+    <!--                    </div>-->
+    <!--                </div>-->
+    <!--                <a class="carousel-control-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev">-->
+    <!--                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+    <!--                    <span class="sr-only">Previous</span>-->
+    <!--                </a>-->
+    <!--                <a class="carousel-control-next" href="#carouselExampleIndicators1" role="button" data-slide="next">-->
+    <!--                    <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+    <!--                    <span class="sr-only">Next</span>-->
+    <!--                </a>-->
+    <!--            </div>-->
+    <!--        </div>-->
+    <!--    </div>-->
+
     <?php foreach ($items as $key => $item): ?>
         <div class="row">
+
             <div class="col-12 col-sm-5">
                 <h3><?php echo $item->title; ?></h3>
-<!--                <img src="/--><?php //echo $item->notes; ?><!--" width="100%" class="col-10"><br>-->
 
-                <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
+                <div id="carouselExampleInterval<?php echo $item->id; ?>" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
-                        <?php for ($i = 0; $i < $items->quantity; $i++): ?>
-                        <li data-slide-to="<?php ($i - 1); ?>" <?php if ($i === 0) {?> class="active" <?php } ?>></li>
-<!--                        <li data-slide-to="0" class="active"></li>-->
-<!--                        <li data-slide-to="1"></li>-->
-<!--                        <li data-slide-to="2"></li>-->
+                        <?php for ($i = 0; $i < $item->quantity; $i++): ?>
+                            <li data-target="#carouselExampleIndicators1<?php echo $item->id; ?>"
+                                data-slide-to="<?php echo $i; ?>" <?php if ($i === 0) {
+                                $b++; ?> class="active" <?php } ?>>
+                            </li>
                         <?php endfor; ?>
                     </ol>
+
                     <div class="carousel-inner">
-
                         <?php $temp = $item->notes; ?>
-                        <?php for ($i = 0; $i < 3; $i++): ?>
-                        <div class="carousel-item <?php if ($i === 0) {?> active <?php } ?>" data-interval="200000">
-
-<!--                            --><?php
-//                            $temp = stristr($temp, ',', true);
-//                            echo  $temp;
-//                            ?>
-
-                            <img src="/<?php stristr($temp, ',', true); ?>" class="d-block w-100">
-                            <?php $temp = stristr($temp, ',');
-                            $temp = substr($temp, 1);
-                            ?>
-
-                        </div>
+                        <?php for ($i = 0; $i < $item->quantity; $i++): ?>
+                            <div class="carousel-item <?php if ($i === 0) { ?> active <?php } ?>"
+                                 data-interval="200000">
+                                <img src="/<?php echo stristr($temp, ',', true); ?>" class="d-block w-100">
+                                <?php $temp = stristr($temp, ',');
+                                $temp = substr($temp, 1);
+                                ?>
+                            </div>
                         <?php endfor; ?>
-
-<!--                        <div class="carousel-item active" data-interval="200000">-->
-<!--                            <img src="/foto/livingRoom.jpg" class="d-block w-100">-->
-<!--                        </div>-->
-<!--                        <div class="carousel-item" data-interval="200000">-->
-<!--                            <img src="/foto/slipingRoom.jpg" class="d-block w-100">-->
-<!--                        </div>-->
-<!--                        <div class="carousel-item" data-interval="200000">-->
-<!--                            <img src="/foto/childrenRoom.gif" class="d-block w-100">-->
-<!--                        </div>-->
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
+
+                    <a class="carousel-control-prev" href="#carouselExampleInterval<?php echo $item->id; ?>"
+                       role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="sr-only">Previous</span>
                     </a>
-                    <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
+                    <a class="carousel-control-next" href="#carouselExampleInterval<?php echo $item->id; ?>"
+                       role="button" data-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
                     </a>
+
                 </div>
             </div>
 
             <div class="col-12 col-sm-7">
                 <div class="worksDescription">
-                    <?php
-                    $temp = $item->content;
+                    <?php $temp = $item->content;
                     while (strpos($temp, '*') > 0) :
                         ?>
                         <p><?php
@@ -81,15 +131,18 @@
                             ?>
                         </p>
                     <?php endwhile; ?>
-
                 </div>
-                <!--            <a href="-->
+                <!--                            <a href="-->
+                <!--                -->
                 <?php //echo \yii\helpers\Url::to(['/buy/presentation', 'singerId'=>$item->id]) ?><!--">-->
-                <!--                <button type="button" class="btn btn-outline-success">Buy</button>-->
-                <!--            </a>-->
+                <!--                                <button type="button" class="btn btn-outline-success">Buy</button>-->
+                <!--                            </a>-->
             </div>
+
         </div>
     <?php endforeach; ?>
 </div>
-<!--<script src="/docs/4.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>-->
+<script src="/docs/4.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o"
+        crossorigin="anonymous"></script>
 
