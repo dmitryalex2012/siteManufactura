12e7726656b220a0bbda9a188cea8f9d747b8709
DmitryAlex2012 <tpmfd27@gmail.com>
2020-02-13 16:41:04 +0200

    Finish with Google Maps.

--- views/site/contact.php
+++ views/site/contact.php
@@ -23,18 +23,20 @@ echo "<br>";
     <div id="map"></div>
 </div>
 <script>
-    var map;
-    var marker;
+    let map;
+    let marker;
     function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 50.466384, lng: 30.615957},
+            center: {lat: 50.466282, lng: 30.615236},
             zoom: 15
         });
-        marker = google.maps.Marker({
-            position: {lat: 50.466384, lng: 30.615957},
-            map: map
-            // title: "Дизайн-бюро 'Мануфактура'"
+        marker = new google.maps.Marker({
+            position: {lat: 50.466282, lng: 30.615236},
+            map: map,
+            title: "Дизайн-бюро 'Мануфактура'",
+            label: "Дизайн-бюро 'Мануфактура'"
         });
+
     }
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJhXz9qSSw8KjSOnjMW8b1qeD1AfRmU-4&callback=initMap"
