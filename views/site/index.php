768cf2781297d6bd82a42829308e2957ca676b79
DmitryAlex2012 <tpmfd27@gmail.com>
2020-02-20 15:22:02 +0200

    Begin work with MAIN page.

--- views/site/index.php
+++ views/site/index.php
@@ -6,24 +6,8 @@ use yii\helpers\Html;
 <h1>This page is not ready yet.</h1>
 <?php
 echo Html::a('Перейти', '/products/pillows', ['class'=>'btn btn-lg btn-primary']);
-echo "<br>";
 ?>
 <!--/services/list-->
-
-<div class="card" style="width: 18rem;">
-    <div class="card-body">
-        <!--        <h5 class="card-title">Название карточки</h5>-->
-        <!--        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-        <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
-    </div>
-    <img src="foto/servicesDesignerСall.jpg" class="card-img-top" alt="...">
-<!--    <div class="card-body">-->
-<!--        <h5 class="card-title">Название карточки</h5>-->
-<!--        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-<!--        <a href="#" class="btn btn-primary">Переход куда-нибудь</a>-->
-<!--    </div>-->
-</div>
-
 
 <!--<a href="/products/list">-->
 <!--    <button>To products page</button>-->
