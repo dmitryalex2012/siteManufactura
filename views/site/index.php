f726608478aa09bc576bba89cf5fb1890b3be775
dmitryalex2012 <tpmfd27@gmail.com>
2020-02-23 22:38:37 +0200

    Work with "Contact" item.

--- views/site/index.php
+++ views/site/index.php
@@ -2,6 +2,21 @@
 
 <h1>This page is not ready yet.</h1>
 
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <!--        <h5 class="card-title">Название карточки</h5>-->
+        <!--        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+        <a href="#" class="btn btn-primary">Переход куда-нибудь</a>
+    </div>
+    <img src="foto/servicesDesignerСall.jpg" class="card-img-top" alt="...">
+<!--    <div class="card-body">-->
+<!--        <h5 class="card-title">Название карточки</h5>-->
+<!--        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+<!--        <a href="#" class="btn btn-primary">Переход куда-нибудь</a>-->
+<!--    </div>-->
+</div>
+
+
 <!--<a href="/products/list">-->
 <!--    <button>To products page</button>-->
 <!--</a>-->
