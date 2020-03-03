ddabaec3c1d52d540ac2bce0e773ba6d41dac477
DmitryAlex2012 <tpmfd27@gmail.com>
2020-03-03 18:12:49 +0200

    Work with facade "Cards" in MAIN page.

--- views/site/index.php
+++ views/site/index.php
@@ -3,29 +3,125 @@
 use yii\helpers\Html;
 ?>
 
-<h1>This page is not ready yet.</h1>
 <?php
-//echo Html::a('Перейти', '/products/pillows', ['class'=>'btn btn-lg btn-primary']);
-//echo "<br>";
+/* @var $ourOffers object */
 ?>
-<!--/services/list-->
 
-<div class="card" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title">Название карточки</h5>
-        <?php echo HTML::a('Detail', '/products/pillows', ['class'=>'myBtn btn-lg btn-primary']); echo "<br>"; echo "<br>"; ?>
-        <?php echo Html::a('Перейти', '/products/pillows', ['class'=>'btn btn-lg btn-primary']); ?>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-<!--        <a href="#" class="btn btn-primary">Переход куда-нибудь</a>-->
+
+<?php foreach ($ourOffers as $key=>$ourOffer):
+?>
+<div class="row">
+    <div class="card text-white col-12 col-lg-6">
+        <img src="foto\indexFoto\example4.jpg" class="card-img" alt="100%">
+        <div class="card-img-overlay">
+            <p class="pInIndex"><?php echo $ourOffer->inscription; ?></p>
+            <?php echo Html::a('Выбрать модель', '/products/pillows', ['id'=>"myBtn", 'class'=>'btn btn-outline-info']); ?>
+        </div>
     </div>
-    <img src="foto/servicesDesignerСall.jpg" class="card-img-top" alt="...">
-<!--    <div class="card-body">-->
-<!--        <h5 class="card-title">Название карточки</h5>-->
-<!--        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-<!--        <a href="#" class="btn btn-primary">Переход куда-нибудь</a>-->
-<!--    </div>-->
-</div>
 
+    <div class="card text-white col-12 col-lg-6">
+        <img src="foto\indexFoto\example4.jpg" class="card-img" alt="100%">
+        <div class="card-img-overlay">
+            <p class="pInIndex">Постельное белье</p>
+            <?php echo Html::a('Выбрать модель', '/products/pillows', ['id'=>"myBtn", 'class'=>'btn btn-outline-info']); ?>
+        </div>
+    </div>
+
+    <div class="card text-white col-12 col-lg-6">
+        <img src="foto\indexFoto\example4.jpg" class="card-img" alt="100%">
+        <div class="card-img-overlay">
+            <p class="pInIndex">Постельное белье</p>
+            <?php echo Html::a('Выбрать модель', '/products/pillows', ['id'=>"myBtn", 'class'=>'btn btn-outline-info']); ?>
+        </div>
+    </div>
+
+    <div class="card text-white col-12 col-lg-6">
+        <img src="foto\indexFoto\example4.jpg" class="card-img" alt="100%">
+        <div class="card-img-overlay">
+            <p class="pInIndex">Постельное белье</p>
+            <?php echo Html::a('Выбрать модель', '/products/pillows', ['id'=>"myBtn", 'class'=>'btn btn-outline-info']); ?>
+        </div>
+    </div>
+</div>
+<?php endforeach;
+?>
+
+
+<!--<h4>Индивидуальный дизайн и пошив штор</h4>-->
+<!--<h4>Все типы жалюзей и ролет</h4>-->
+<!--<h4>Стильные подушки</h4>-->
+<!--<p class="pInIndex">Постельное белье</p>-->
+<!--<p class="pInIndex">Клевые полотенца для всей семьи</p>-->
+<!--<h4>Apero</h4>-->
+
+
+
+
+<!--<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">-->
+<!--    <ol class="carousel-indicators">-->
+<!--        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>-->
+<!--        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>-->
+<!--        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>-->
+<!--        <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>-->
+<!--        <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>-->
+<!--        <li data-target="#carouselExampleCaptions" data-slide-to="5"></li>-->
+<!--    </ol>-->
+<!--    <div class="carousel-inner">-->
+<!--        <div class="carousel-item active">-->
+<!--            <img src="foto/servicesDelivery.jpg" class="d-block w-100" alt="100%">-->
+<!--            <div class="carousel-caption d-none d-md-block">-->
+<!--                <h4>Индивидуальный дизайн и пошив штор</h4>-->
+<!--                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="carousel-item">-->
+<!--            <img src="foto/servicesDesignProgect.jpg" class="d-block w-100" alt="100%">-->
+<!--            <div class="carousel-caption d-none d-md-block">-->
+<!--                <h4>Все типы жалюзей и ролет</h4>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="carousel-item">-->
+<!--            <img src="foto/servicesTextileSelection.jpg" class="d-block w-100" alt="100%">-->
+<!--            <div class="carousel-caption d-none d-md-block">-->
+<!--                <h4>Стильные подушки</h4>-->
+<!--                --><?php //echo Html::a('Выбрать модель', '/products/pillows', ['id'=>"myBtn", 'class'=>'btn btn-outline-info']); ?>
+<!--                <br> <br>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="carousel-item">-->
+<!--            <img src="foto\indexFoto\example4.jpg" class="d-block w-100" alt="100%">-->
+<!--            <div class="carousel-caption d-none d-md-block">-->
+<!--                <p class="pInIndex">Постельное белье</p>-->
+<!--                --><?php //echo Html::a('Выбрать модель', '/products/pillows', ['id'=>"myBtn", 'class'=>'btn btn-outline-info']); ?>
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="carousel-item">-->
+<!--            <img src="foto\indexFoto\example4.jpg" class="d-block w-100" alt="100%">-->
+<!--            <div class="carousel-caption d-none d-md-block">-->
+<!--                <p class="pInIndex">Клевые полотенца для всей семьи</p>-->
+<!--                --><?php //echo Html::a('Выбрать модель', '/products/pillows', ['id'=>"myBtn", 'class'=>'btn btn-outline-info']); ?>
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="carousel-item">-->
+<!--            <img src="foto/servicesDesignProgect.jpg" class="d-block w-100" alt="100%">-->
+<!--            <div class="carousel-caption d-none d-md-block">-->
+<!--                <h4>Apero</h4>-->
+<!--                --><?php //echo Html::a('Выбрать модель', '/products/pillows', ['id'=>"myBtn", 'class'=>'btn btn-outline-info']); ?>
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">-->
+<!--        <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+<!--        <span class="sr-only">Previous</span>-->
+<!--    </a>-->
+<!--    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">-->
+<!--        <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+<!--        <span class="sr-only">Next</span>-->
+<!--    </a>-->
+<!--</div>-->
+
+
+<!--/services/list-->
 
 <!--<a href="/products/list">-->
 <!--    <button>To products page</button>-->
