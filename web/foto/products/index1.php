534bb2ba6d712119ceefaaa993e678f9bd93ffaa
dmitryalex2012 <tpmfd27@gmail.com>
2020-03-15 22:56:12 +0200

    Begin load files on hosting.

--- web/foto/products/index1.php
+++ web/foto/products/index1.php
@@ -1,11 +1,19 @@
+<!DOCTYPE html>
 <html>
-<head>
-	<title>Главная страница - страница обо мне</title>
-</head>
-<body>
-<h1>Информация обо мне</h1>
-<br>
-<hr>
-<h1>Информация обо мне</h1>
-</body>
+	<head>
+		<meta chrset="UTR-8">
+		<title>First page</title>
+		<link rel="stylesheet" href="./style.css">		
+	</head>
+	<body>
+		<div id="header">
+			<ul>
+				<li><a href="#">About us</a></li>
+				<li><a href="#">Contacts</a></li>
+				<li><a href="#">Products list</a></li>
+				<li><a href="#">Gallary</a></li>
+			</ul>
+		</div>	
+	</body>
+
 </html>
