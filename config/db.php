843d57a3f5d453f5e7a455b910ae3b25c6954ef5
DmitryAlex2012 <tpmfd27@gmail.com>
2020-07-16 17:10:21 +0300

    Service information (DB name, user name etc) is added. Adapted for CEO code is loaded on hosting.

--- config/db.php
+++ config/db.php
@@ -3,12 +3,9 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+//    'dsn' => 'mysql:host=curtains.mysql.tools;dbname=curtains_curtain', // for  http://www.designburoshtor.website/
     'username' => 'sitemanufactura',
+//    'username' => 'curtains_curtain',                                   // for  http://www.designburoshtor.website/
     'password' => '200384',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
